57d9de9343daaf4675529e9dc9a36ff24b375dd4
Tanner Eustice <teustice@msn.com>
2017-04-28 08:56:52 -0700

    add logic to pass second spec
    spec verifies object is being pushed into a class variable

--- lib/word.rb
+++ lib/word.rb
@@ -1,7 +1,18 @@
 class Word
   attr_accessor(:word, :definitions)
+  @@words = []
+
   def initialize(word)
     @word = word
     @definitions = []
+    @@words.push(word)
+  end
+
+  def Word.all
+    @@words
+  end
+
+  def Word.clear
+    @@words = []
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,10 +4,19 @@ require "pry"
 
 
 describe(Word) do
+  before do
+    Word.clear
+  end
+  
   describe("Word#initialize") do
     it("initializes object with it's properties") do
       new_word = Word.new("candle")
       expect(new_word.word).to(eq("candle"))
+    end
+
+    it("pushes the word into a class variable") do
+      Word.new("candle")
+      expect(Word.all[0]).to(eq("candle"))
     end
   end
 end
